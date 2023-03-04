0f6eec434f02b4b9934467de9a8b0a40ea5e6dd3
paul-madut <paul.madut@mths.ca>
2023-03-04 04:03:59 -0500

    add: Welcome page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,20 @@
-<div class="prose">
-  <h1>Welcome to SvelteKit</h1>
-  <p>
-    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+<script>
+  import image from "$lib/assets/secret_sauce.png";
+</script>
+
+<div class=" flex flex-col justify-center items-center h-screen">
+  <h1 class=" text-center mt-4 text-8xl text-white pb-4">File Phantom</h1>
+  <p class="text-xl text-gray-400 text-center pb-4 mb-10">
+    Store your files fast, safe and anonymously!
   </p>
 
-  <button class="btn"> Hello </button>
+  <button
+    class="btn drop__container flex flex-col justify-center h-1/3 w-2/3 bg-white hover:bg-slate-200 rounded-3xl shadow-2xl "
+  >
+    <h2 class="text-center cursor-default">
+      Drop a file or <span class=" text-blue-600 cursor-pointer"> Click </span>
+    </h2>
+  </button>
+
+  <img class="h-1/3" src={image} alt="secret sauce" />
 </div>
