22bee8dff5f86c71e80b3a801d8234733e20f7f3
Fernando Nogueira <abert036@uottawa.ca>
2023-03-04 20:46:07 +0100

    chore: update theme config

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,18 +3,27 @@
 </script>
 
 <div class=" flex flex-col justify-center items-center h-screen">
-  <h1 class=" text-center mt-4 text-8xl text-white pb-4">File Phantom</h1>
-  <p class="text-xl text-gray-400 text-center pb-4 mb-10">
+  <h1 class="  mt-4 text-8xl pb-4">File Phantom</h1>
+  <p class="text-xl text-gray-400 pb-4 mb-10">
     Store your files fast, safe and anonymously!
   </p>
 
-  <button
-    class="btn drop__container flex flex-col justify-center h-1/3 w-2/3 bg-white hover:bg-slate-200 rounded-3xl shadow-2xl "
-  >
-    <h2 class="text-center cursor-default">
+  <button class="btn btn-primary h-1/3 w-2/3 rounded-3xl shadow-2xl ">
+    <h2 class="text-primary-content">
       Drop a file or <span class=" text-blue-600 cursor-pointer"> Click </span>
     </h2>
   </button>
-
-  <img class="h-1/3" src={image} alt="secret sauce" />
 </div>
+
+<img class="bottom-art h-1/3" src={image} alt="secret sauce" />
+
+<style>
+  .bottom-art {
+    /* Centered bottom middle */
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    transform: translate(-50%, 0);
+    pointer-events: none;
+  }
+</style>
